33db6616131155891a95f04241bd2ab320d7b963
yotam18-meet <yotamasael7@gmail.com>
2017-01-09 19:46:05 +0200

    Worked on Inheiritence

--- my_str.py
+++ my_str.py
@@ -1,5 +1,5 @@
 #FIX THE LINE BELOW
-class xyz : #<-----Replace xyz-make a new class, MyStr, that inherits from str
+class MyStr(str) : #<-----Replace xyz-make a new class, MyStr, that inherits from str
     """
     Build a subclass of str with some new, fun methods.
     """
@@ -49,4 +49,6 @@ class xyz : #<-----Replace xyz-make a new class, MyStr, that inherits from str
         #       do this in as little as 1 line.
         #YOUR CODE BELOW:
         #################
+        variable=super(MyStr,MyStr(self.lower())).replace(take_out.lower(), use_this)
+        return variable.lower() 
         
